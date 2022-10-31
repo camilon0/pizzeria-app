35039b55cce2070362e9d45649539f56805fbde9
Ana-Rojas88 <anac.rojas88@gmail.com>
2022-10-31 16:27:21 -0500

    login and desing

--- src/components/loginAndRegister/Login.jsx
+++ src/components/loginAndRegister/Login.jsx
@@ -1,11 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
 const Login = () => {
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const userLogin = () => {
+    console.log('iniciar sesión')
+  }
+
   return (
-    <div>
-      <Link to="/register">
-        <button>Register</button>
-      </Link>
+    <div className="login">
+      <img src="https://hungryforhalaal.co.za/wp-content/uploads/2021/05/Pizza-Spots-Cape-Town-Hungry-for-Halaal.jpg" alt="Pizza" />
+      <div className="login__title">
+        <h1>PizzaApi</h1>
+        <h2>Inicia sesión en tu cuenta</h2>
+        <p>Disfruta de la mejor pizza creada para las personas amantes del Código.</p>
+      </div>
+      <form onSubmit={handleSubmit(userLogin)} className="form">
+        <div className="input">
+          <input type="email" placeholder="Email"
+            {...register("email", { required: true })}
+            className={errors.email ? "input--error" : ""}
+          />
+        </div>
+
+        <div className="input">
+          <input type="password" placeholder="Contraseña"
+            {...register("password", { required: true })}
+            className={errors.password ? "input--error" : ""} />
+        </div>
+        <button>Iniciar sesión</button> 
+      </form>
+      <div className="linkRegister">
+      <span>Restablecer contraseña</span> 
+        <h5>¿No tienes una cuenta?</h5>
+        <Link to="/register">
+          <button className="register">Registrate aquí</button>
+        </Link>
+        </div>
+      
     </div>
   );
 };
--- src/components/loginAndRegister/Register.jsx
+++ src/components/loginAndRegister/Register.jsx
@@ -1,11 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import './style.scss'
+
 const Register = () => {
+
+const { register, handleSubmit, formState: { errors } } = useForm();
+ 
+const userLogin = () =>{
+    console.log('iniciar sesión')
+}
+
   return (
-    <div>
-      <Link to="/">
-        <button>Login</button>
+    <div className="login">
+      <img src="https://hungryforhalaal.co.za/wp-content/uploads/2021/05/Pizza-Spots-Cape-Town-Hungry-for-Halaal.jpg" alt="Pizza" />
+       <form onSubmit={handleSubmit(userLogin)} className="form">
+       <label>
+            Nombre:
+            <input type="text" 
+            {...register("name", { required: true})}
+            className={errors.name ? "input--error" : ""}
+             />
+        </label>
+        <label>
+            Email:
+            <input type="email" 
+            {...register("email", { required: true})}
+            className={errors.email ? "input--error" : ""}
+             />
+        </label>
+        <label>
+            Password:
+            <input type="password" 
+            {...register("password", { required: true })}
+            className={errors.password ? "input--error" : ""}/>
+        </label>
+        <Link to="/">
+        <button>Registrarse</button>
       </Link>
+    </form>
+     
     </div>
   );
 };
