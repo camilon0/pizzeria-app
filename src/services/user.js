2d2b66828dbd8addf9bd3ee98c7d5f4be0efe04f
DaniaAlvarado <dania26alvarado@gmail.com>
2022-11-01 16:20:07 -0500

    home

--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const URL_USERS = "http://localhost:5000/users";
+const URL_USERS = " http://localhost:3004/users";
+const URL_PIZZAS = 'http://localhost:3004';
 
 export const userFind = async (email, pass) => {
   const url = `${URL_USERS}?email=${email}&password=${pass}`;
@@ -17,6 +18,18 @@ export const userFind = async (email, pass) => {
 export const createUser = async (user) => {
   try {
     const { data } = await axios.post(`${URL_USERS}`, user);
+    return data;
+  } catch (error) {
+    return {
+      error,
+      data: null,
+    };
+  }
+};
+
+export const getPizzas = async () => {
+  try {
+    const { data } = await axios.get(`${URL_PIZZAS}/pizzas`);
     return data;
   } catch (error) {
     return {
